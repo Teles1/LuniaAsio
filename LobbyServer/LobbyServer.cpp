f02fa6b7823047f7511eb66c78dfc0c92525a81b
Teles <mteleshenrique@gmail.com>
2020-12-08 16:08:37 -0500

    *Logger Sigleton created *Replaces the macros with the new Log Singleton

--- LobbyServer/LobbyServer.cpp
+++ LobbyServer/LobbyServer.cpp
@@ -3,15 +3,17 @@
 namespace Lobby {
 	void LobbyServer::HandleNewConnection(const asio::error_code& err_code, asio::ip::tcp::socket& socket)
 	{
-		INFO_LOG("Connection handled by Lobby");
 		net::UserRegistry::GetInstance()->MakeUser(socket)->HandleRead();
+		Logger::GetInstance()->Info("Connection handled by Lobby");
 	}
 }
 int main(int argc, char* argv[])
 {
+	//setting log name to be used on the console.
+	Logger::GetInstance("LobbyServer");
 	//Load Config
 	Lobby::LobbyServer lobbyServer(Lobby::Config.ServerIp.c_str(), Lobby::Config.ServerPort);
-	INFO_LOG("{0} Initialized on port {1}", Lobby::Config.ServerName, Lobby::Config.ServerPort);
+	Logger::GetInstance()->Info("{0} Initialized on port {1}", Lobby::Config.ServerName, Lobby::Config.ServerPort);
 	lobbyServer.Run();
 	return 0;
 }
