b8b90bd5c6919365f56dd9b07eab092bef344dfb
Teles <mteleshenrique@gmail.com>
2020-12-17 05:08:13 -0500

    *Moved the PositionData out of the CompressedLoader

--- Info/Info/CompressedLoader.h
+++ Info/Info/CompressedLoader.h
@@ -8,6 +8,19 @@ namespace Lunia {
 	namespace XRated {
 		namespace Database {
 			namespace Info {
+				template <typename Template>
+				struct PositionData : public Serializer::ISerializable
+				{
+					Template dataPosition;
+					virtual void Serialize(Serializer::IStreamWriter& out) const
+					{
+						out.Write(L"dataPosition", dataPosition);
+					}
+					virtual void Deserialize(Serializer::IStreamReader& in)
+					{
+						in.Read(L"dataPosition", dataPosition);
+					}
+				};
 
 				class CompressedLoader {
 				public:
@@ -84,20 +97,6 @@ namespace Lunia {
 						delete [] Buffer;
 					}
 				};
-				template <typename Template>
-				struct PositionData : public Serializer::ISerializable
-				{
-					Template dataPosition;
-					virtual void Serialize(Serializer::IStreamWriter& out) const
-					{
-						out.Write(L"dataPosition", dataPosition);
-					}
-					virtual void Deserialize(Serializer::IStreamReader& in)
-					{
-						in.Read(L"dataPosition", dataPosition);
-					}
-				};
-
 			}
 		}
 	}
