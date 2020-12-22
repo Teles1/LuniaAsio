dd024aacc90844e2db38f47e9099d693828cf54e
slashkeyvalue <felipekvalue@gmail.com>
2020-12-22 08:22:45 -0300

    Add basic members for ClientProxy and ClientProxyLobby

--- Core/GameServer/Proxy/ClientProxy.h
+++ Core/GameServer/Proxy/ClientProxy.h
@@ -8,10 +8,41 @@ struct ClientProxy : public ClientNetworkIO //, TODO? std::enable_shared_from_th
 
 	~ClientProxy() { };
 
+	uint32_t GetId() const
+	{
+		return this->m_id;
+	}
+
+	void SetId(const uint32_t& id)
+	{
+		this->m_id = id;
+	}
+
+	void SetLocale(const std::wstring& inLocale);
+
+	void SetActiveAccountName(const std::wstring& inAccountName);
+
+	std::wstring GetActiveAccountName() const;
+
+	/* Old Name: QueryAliveAuth */
+	bool SatisfyAlivePingWait();
+
+	/* Old Name: CheckAliveAuth */
+	bool IsWaitingOnAlivePing() const;
+
+	/* Old Name: UpdateAliveAuth */
+	// void SetAliveAsLastTickAlivePing(const Alive::AliveData& answer);
+
 public:
 	fwEvent<> OnDropped;
 
 private:
 
-	uint32_t id;
+	uint32_t m_id;
+
+	bool m_waitingOnAlivePing = false;
+
+	std::wstring m_locale;
+
+	std::wstring m_accountName;
 };
--- LobbyServer/GameServer/ClientProxy/ClientProxyLobby.h
+++ LobbyServer/GameServer/ClientProxy/ClientProxyLobby.h
@@ -5,4 +5,14 @@
 struct ClientProxyLobby : public ClientProxy
 {
 	ClientProxyLobby(asio::ip::tcp::socket&& socket); /* WARN socket is moved a shit ton of times here */
+
+	bool IsAuthenticated() const;
+
+	bool HasSecondPasswordAuthentication() const;
+
+private:
+
+	bool m_isAuthenticated = false;
+
+	bool m_hasSecondPasswordAuthentication = false;
 };
