9767d2c769db96427558511d1b89c4b3841a7089
Crazy <mtfactions@gmail.com>
2020-12-25 00:50:17 -0300

    * Updated Hardcode LoL

--- Info/Info/StageInfo/StageInfoManager.cpp
+++ Info/Info/StageInfo/StageInfoManager.cpp
@@ -86,30 +86,18 @@ namespace Lunia {
 					int index;
 					for (; i != stages.end(); ++i) {
 						StageInfo& info = (*i).second;
-						String tempChar = info.MoveMapName;
-						// Removing /Move from name folder
-						std::wstring substring = L"/Move";
-						std::size_t pos = tempChar.find(substring); 
-						if (pos != std::string::npos) {
-							tempChar.erase(pos, substring.length());
-						}
-
-						// Changing .move to .xml and Move with uppercase what???
-						std::wstring StringMove = L".move";
+						String tempChar = info.ScriptName;
+						// Changing .move to .xml
+						std::wstring StringMove = L".as";
 						std::size_t position = tempChar.find(StringMove);
 						if (position < 100) {
 							tempChar.replace(position, StringMove.length(), L".xml");
-						}
-						else {
-							std::wstring StringMove = L".Move";
-							std::size_t position = tempChar.find(StringMove);
-							tempChar.replace(position, StringMove.length(), L".xml");
-						}
+						} 
 
 						Resource::SerializerStreamWriter writer = Resource::ResourceSystemInstance().CreateSerializerXmlStreamWriter(tempChar.c_str());
 						writer->Write(L"Stage", info);
-						std::wcout << L"Stage Done...\n";
 					}
+					std::wcout << L"Stage Done...\n";
 				}
 
 				void StageInfoManager::LoadPvpItemStageInfo()
