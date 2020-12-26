75a20d376023d188287c120fe936ccf1eb31ae57
Crazy <mtfactions@gmail.com>
2020-12-26 14:07:47 -0300

    * Updated Info

--- Info/Info.cpp
+++ Info/Info.cpp
@@ -10,7 +10,7 @@ namespace Lunia {
 				return m_Instance;
 			};
 			void LuniaDatabase::Init() {
-				InfoCollections.npcs.Init(false);
+				//InfoCollections.npcs.Init(false);
 				//InfoCollections.stageInfos.Init(false);
 				//InfoCollections.actions.Init(false);
 				//InfoCollections.npcs.Init(false);
--- Info/Info.h
+++ Info/Info.h
@@ -1,17 +1,20 @@
 #pragma once
 
 #include <Core/Core.h>
-#include <Info/Info/Database.h>
+#include "./Info/Database.h"
 #include <Info/Info/Items/CompressedItemInfoManager.h>
-#include <Info/Info/GlobalInfo.h>
-#include <Info/Info/NPCs/NPCManager.h>
-#include <Info/Info/Actions/ActionsManager.h>
-#include <Info/Info/StateBundles/StateBundlesManager.h>
-#include <Info/Info/StageGroup/StageGroupManager.h>
-#include <Info/Info/StageInfo/StageInfoManager.h>
+#include "Info/GlobalInfo.h"
+#include "Info/NPCs/NPCManager.h"
+#include "Info/Actions/ActionsManager.h"
+#include "Info/StateBundles/StateBundlesManager.h"
+#include "Info/StageGroup/StageGroupManager.h"
+#include "Info/StageInfo/StageInfoManager.h"
 #include <Info/Info/Enchant/EnchantTable.h>
 #include <Info/Info/EquipSet/EquipSetManager.h>
 #include <Info/Info/Pets/PetsManager.h>
+#include <Info/Info/PvpRewardItemInfos/PvPRewardItemInfosManager.h>
+#include <Info/Info/Event/EventInfoManager.h>
+#include <Info/Info/StylePoints/StylePointInfoManager.h>
 
 namespace Lunia {
 	namespace XRated {
@@ -25,12 +28,15 @@ namespace Lunia {
 						Info::StageGroupManager StageGroups;
 						Info::NPCInfoManager Npcs;
 						Enchant::EnchantTableManager EnchantTables;
-						//Info::StageInfoManager stageInfos;
-						//Info::ActionInfoManager actions;
 						Info::StateBundleInfoManager StateBundles;
 						Info::CompressedItemInfoManager Items;
 						Info::EquipSetManager EquipSet;
 						Info::PetManager Pets;
+						Info::StageInfoManager StageInfos;
+						Info::ActionInfoManager Actions;
+						Info::PvPRewardItemTable PvPRewardItemInfos;
+						Info::EventInfoManager EventInfoManager;
+						Info::StylePointInfoManager StylePoints;
 					} InfoCollections;
 			};
 			LuniaDatabase& DatabaseInstance();
