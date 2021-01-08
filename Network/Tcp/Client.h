e56a4557df640413a66b6db88d1d0e24a5950202
Teles <mteleshenrique@gmail.com>
2021-01-07 20:29:09 -0500

    *Making m_floodChecker Protected instead of private

--- Network/Tcp/Client.h
+++ Network/Tcp/Client.h
@@ -17,7 +17,6 @@ namespace Lunia {
                 void SetCryptoKey(uint32& newKey);
                 uint32 GetCryptoKey();
                 const std::string GetPeerAddress() const;
-
                 Crypt::Box		m_decryptor;
 
             protected:
@@ -25,11 +24,11 @@ namespace Lunia {
                 void ReceivedSome(const error_code& ec, size_t size);
                 void WroteSome(const error_code& error, size_t size);
                 virtual uint32 Parse(uint8* buffer, size_t size) = 0;
+                FloodChecker            m_floodChecker;
             private:
                 bool					m_isEncryptKey;
                 uint8					m_buffer[READ_DATA_BUFFER_LENGTH] = {};
                 tcp::socket				m_socket;
-                FloodChecker            m_floodChecker;
             };
         }
     }
